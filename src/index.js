9a077153e8e903a742e9727691aeeaa02bc9850f
FranSanabri <death.the.system@gmail.com>
2023-05-19 14:22:45 -0300

    Cinema

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import axios from "axios";
+
+axios.defaults.baseURL = 'http://localhost:3001';
 
 ReactDOM.render(
     <BrowserRouter>
